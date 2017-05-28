ebcd223b4601ebc246101eb50a1a2877ee2eecba
Arthur de Jesus <adejesus@ucsd.edu>
2017-05-27 18:17:17 -0700

    modified gitignore to include java files

---
+++ src/ucsd/shoppingApp/ConnectionManager.java
@@ -0,0 +1,34 @@
+package ucsd.shoppingApp;
+
+import java.sql.*;
+
+public class ConnectionManager {
+	public static Connection con = null;
+	
+	public ConnectionManager(){	
+	}
+	
+	public static Connection getConnection() {
+		try {
+			 Class.forName("org.postgresql.Driver");
+			
+            con = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost/Tatsu?" +
+                    "user=postgres&password=postgres1");
+            con.setAutoCommit(false);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	public static void closeConnection(Connection con) {
+		try {
+			if(con != null) {
+				con.close();
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
